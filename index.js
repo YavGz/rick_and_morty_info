608113bc30890fa4a46961abfeb01e9bd372f810
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-10-18 18:27:12 -0500

    Preparando nodos y lazy loading

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+// lazy loading -----
+const observer = new IntersectionObserver((entries) => {
+  entries.filter(isIntersecting).forEach(loadImage)
+})
+
+const isIntersecting = (entry) => {
+  return entry.isIntersecting;
+}
+
+const loadImage = (entry) => {
+  const imagen = entry.target;
+  const url = imagen.dataset.src
+  // cargar imagen
+  imagen.src = url
+  
+  imagen.onload = () => {
+  }
+  // quita el registro de la imagen (unlisten)
+  observer.unobserve(imagen)
+}
+
+const registerImage = (imagen) => {
+  //intersectationObservador -> observer(imagen)
+  observer.observe(imagen);
+}
+
+// ----- lazy loading
+
 const API = "https://rickandmortyapi.com/api/character/";
 
-const HTMLResponse = document.querySelector('#app')
+const appNode = document.querySelector('#app')
 
 const UL = document.createElement('ul')
 
@@ -8,12 +36,17 @@ fetch(API)
 .then((response) => response.json())
 .then((response) => {
   response.results.forEach( (result) => {
-    let elem = document.createElement('li')
+    const container = document.createElement('div')
+    const wrapper = document.createElement('div')
+    wrapper.className = 'wrapper'
+    const divIMG = document.createElement('li')
+
     elem.appendChild(
       document.createTextNode(`ID: ${result.id}  Nombre: ${result.name}`)
     );
-    UL.appendChild(elem)
+    container.appendChild(wrapper)
+
+    appNode.appendChild(container)
   });
-  HTMLResponse.appendChild(UL)
 })
 .catch( () => {console.log(Response.error);});
